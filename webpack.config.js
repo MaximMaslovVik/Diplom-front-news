98336ddde3875e8878d235afd7edfb991fcd7102
Максим Маслов <flyover31@yandex.ru>
2020-04-27 23:28:50 +0300

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -5,18 +5,35 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WebpackMd5Hash = require('webpack-md5-hash');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
-
+module.exports = {
+  entry: __dirname + "/src/app/index.js", // webpack entry point. Module to start building dependency graph
+  output: {
+    path: __dirname + '/dist', // Folder to store generated bundle
+    filename: 'bundle.js',  // Name of generated bundle after build
+    publicPath: '/' // public URL of the output directory when referenced in a browser
+  },
+  module: {  // where we defined file patterns and their loaders
+      rules: [
+      ]
+  },
+  plugins: [  // Array of plugins to apply to build chunk
+      new HtmlWebpackPlugin({
+          template: __dirname + "/src/public/index.html",
+          inject: 'body'
+      })
+  ],
+  devServer: {  // configuration for webpack-dev-server
+      contentBase: './src/public',  //source of static assets
+      port: 7700, // port to run dev-server
+  }
+};
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[chunkhash].js'
     },
-    entry: { main: './src/secondary.js' },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[chunkhash].js'
-    },
+
     module: {
         rules: [{
                 test: /\.js$/,
@@ -60,11 +77,9 @@ module.exports = {
             filename: 'secondary.html'
         }),
         new MiniCssExtractPlugin({
-            filename: 'style.[contenthash].css'
+            filename: 'index.[contenthash].css'
         }),
-        new MiniCssExtractPlugin({
-          filename: 'secondary.[contenthash].css'
-      }),
+
         new OptimizeCssAssetsPlugin({
             assetNameRegExp: /\.css$/g,
             cssProcessor: require('cssnano'),
