356e4230d81a1f0a813be17c9ae0b48b2a5763f6
Максим Маслов <flyover31@yandex.ru>
2020-04-28 00:11:50 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -11,4 +11,4 @@ if (textHeight < 100) {
 }
 
 import "./index.css";
-import "./seconary.css";
+import "./secondary.css";
---
+++ src/secondary.js
@@ -0,0 +1,13 @@
+const textCard = document.querySelector('.card__paragraph');
+const textHeight = textCard.offsetHeight;
+console.log(textCard.offsetHeight);
+
+const box = textCard.textContent;
+console.log(box);
+
+if (textHeight < 100) {
+    const str = box.substr(0, 121);
+    textCard.textContent = str + "...";
+}
+
+import "./secondary.css"
